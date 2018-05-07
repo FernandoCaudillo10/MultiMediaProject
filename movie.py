0dfad28cd2d3bee35e66b14eafd5f6d7e5ad4ada
Fernando Caudillo <caudillojavier@gmail.com>
2018-05-07 14:56:22 -0700

    created a working image to video creation window

--- movie.py
+++ movie.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QBoxLayout, QLabel, QListWidget, QListWidgetItem
+from PyQt5.QtWidgets import QApplication, QWidget, QBoxLayout, QLabel, QListWidget, QListWidgetItem, QPushButton
 from PyQt5.QtGui import QPixmap, QIcon, QImage, QDrag
 from PyQt5.QtCore import QSize, pyqtSlot
- 
+import cv2 
 from PIL import Image
 
 class Movie(QWidget):
@@ -24,17 +24,17 @@ class Movie(QWidget):
 		self.initWindow()
 		self.createPicIcons()
 		
-
-		self.leftLabel = QLabel()
-		self.layout.addWidget(self.leftLabel)
 	
 		self.picList.itemClicked.connect(self.updateLeftView)
-		
+		self.button.clicked.connect(self.updateList)		
+
 	def initWindow(self):
 
 		self.setGeometry(100,100,2000,1000)
 
 		self.layout = QBoxLayout(QBoxLayout.RightToLeft)
+		
+		self.rightLayout = QBoxLayout(QBoxLayout.TopToBottom)
 
 		self.picList = QListWidget(self)
 		self.picList.setIconSize(QSize(100,100))
@@ -43,9 +43,12 @@ class Movie(QWidget):
 		self.picList.setDropIndicatorShown(True)
 		self.picList.setDragEnabled(True)
 		self.setAcceptDrops(True)
-
-		self.layout.addWidget(self.picList)
-
+		
+		self.button = QPushButton(self)
+		self.rightLayout.addWidget(self.picList)
+		self.rightLayout.addWidget(self.button)
+		
+		self.layout.addLayout(self.rightLayout)
 		self.setLayout(self.layout)
 
 	def createPicIcons(self):
@@ -56,6 +59,9 @@ class Movie(QWidget):
 			item.setStatusTip(p)
 			item.setIcon(icon)
 
+		self.leftLabel = QLabel()
+		self.layout.addWidget(self.leftLabel)
+
 	def updateLeftView(self):
 		self.previousRow = self.picList.currentRow()
 		current = QPixmap(self.picList.currentItem().text())
@@ -63,6 +69,16 @@ class Movie(QWidget):
 		
 	def updateList(self):
 		
+		firstFrame = cv2.imread(self.picList.item(0).text())		
+		fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+		out = cv2.VideoWriter("createdVideo", fourcc, 1.0, (firstFrame.shape[0], firstFrame.shape[1]))
+		
 		for i in range(self.picList.count()):
-			print(self.picList.item(i).text())
+			frame = cv2.imread(self.picList.item(i).text())
+			out.write(frame)
+			
+		out.release()
+		
+
+
 
