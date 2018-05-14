2f872d1aae6e28810a6b38e8f90b79bd417abee2
Fernando Caudillo <caudillojavier@gmail.com>
2018-05-14 12:47:51 -0700

    added respective authors to each file

--- movie.py
+++ movie.py
@@ -3,8 +3,9 @@
 #	Abstract:	This Code creates a program that will allow users to modify an image given
 #				various predefined modifications. Another feature is that of creating a 
 #				slideshow of given images that can be rearranged at any time.
-#	Authors:	FernandoCaudillo, Clement Reau, Andres Gonzalez
 #	Date:	14 May 2018
+
+#	Authors:	FernandoCaudillo
 
 import sys
 import time
